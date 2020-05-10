3311f8e67f428cdbe4456398a3135f4cd781459b
hodisanluissa <56643310+hodisanluissa@users.noreply.github.com>
2020-05-10 15:57:43 +0300

    Add files via upload

---
+++ Agenda/Agenda/Agenda/Create.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System;
+
+namespace Agenda
+{
+    static class Create
+    {
+        public static List<Person> People = new List<Person>();
+        public static List<Activity> Activities = new List<Activity>();
+        public static List<Agend> Agendas = new List<Agend>();
+        public static Person CreatPerson(string lastname,
+            string firstname, DateTime birth, string email)
+        {
+            Person person = new Person()
+            {
+                LastName = lastname,
+                FirstName = firstname,
+                BirthData = birth,
+                EmailAdress = email
+            };
+            People.Add(person);
+            Console.WriteLine($"Peron:{person.Info()} was added");
+            return person;
+        }
+    }
+}
--- Agenda/Agenda/Agenda/Program.cs
+++ Agenda/Agenda/Agenda/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics.Tracing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,11 @@ namespace Agenda
     {
         static void Main(string[] args)
         {
-        }
+            Person Luissa = Create.CreatPerson("Hodisan",
+                "Luissa", new DateTime(2000, 11, 11),"luissa_izabela@yahoo.com");
+            Activity Walking = Luissa.CreateActivity("the dog", "individualy",
+                DateTime.Now, DateTime.Now.AddHours(1));
+
+        } 
     }
 }
